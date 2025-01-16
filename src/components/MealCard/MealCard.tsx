d5a0a1aeda75af3a762a60a821dc288ac9169b82
Airat Vagapov <airlake@yandex.ru>
2025-01-16 23:41:08 +0300

    add link to card

--- src/components/MealCard/MealCard.tsx
+++ src/components/MealCard/MealCard.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import TextLink from '@/ui/TextLink/TextLink'
+import Image from "next/image";
+import TextLink from "@/ui/TextLink/TextLink";
 
-import styles from './MealCard.module.sass'
-import TagGroup from '@/components/TagGroup/TagGroup'
-
+import styles from "./MealCard.module.sass";
+import TagGroup from "@/components/TagGroup/TagGroup";
 
 interface IMealCard {
-    meal: any
-    key?: string | number
+    meal: any;
+    key?: string | number;
 }
 
 const MealCard: React.FC<IMealCard> = ({ key, meal }) => {
@@ -15,26 +14,32 @@ const MealCard: React.FC<IMealCard> = ({ key, meal }) => {
     dataArr.push(meal?.strArea);
     dataArr.push(meal?.strCategory);
 
-
     return (
         <>
-            {meal &&
+            {meal && (
                 <div key={key} className={styles.mealCard}>
-
-                    <img src={meal?.image} alt={meal?.title} />
+                    <a href={`/meals/${meal?.id}`}>
+                        <img src={meal?.image} alt={meal?.title} />
+                    </a>
                     {/* <Image src={meal?.image} width={120} height={120} alt={meal?.title} /> */}
 
                     <div className={styles.mealCardContent}>
-                        <p className={styles.mealTitle}>{meal?.title}</p>
+                        <a href={`/meals/${meal?.id}`}>
+                            <p className={styles.mealTitle}>{meal?.title}</p>
+                        </a>
                         <TagGroup data={dataArr} />
                     </div>
 
                     <div className={styles.mealLink}>
-                        <TextLink size={16} text="Show more" link={`/meals/${meal?.id}`} />
+                        <TextLink
+                            size={16}
+                            text="Show more"
+                            link={`/meals/${meal?.id}`}
+                        />
                     </div>
                 </div>
-            }
+            )}
         </>
-    )
-}
+    );
+};
 export default MealCard;
