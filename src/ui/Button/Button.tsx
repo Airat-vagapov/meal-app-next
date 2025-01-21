0488eb83093eecb95a6e837d2f05065a129de8ea
Airat Vagapov <airlake@yandex.ru>
2025-01-21 23:14:43 +0300

    fix

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,7 +1,7 @@
 import styles from './Button.module.sass'
 
-const Button = ({ children, type }: { children: React.ReactNode, type: string }) => {
-    return <button type={type} className={styles.button}>{children}</button>;
+const Button = ({ children, type }: { children: React.ReactNode, type?: string }) => {
+    return <button  className={styles.button}>{children}</button>;
 };
 
 export default Button;
