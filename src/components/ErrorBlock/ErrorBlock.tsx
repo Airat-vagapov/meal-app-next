bca692b67dfbfd3aa49e9cc57d18e57621542db1
airat-vagapov <airlake@yandex.ru>
2025-01-25 15:47:44 +0300

    error block fixes

--- src/components/ErrorBlock/ErrorBlock.tsx
+++ src/components/ErrorBlock/ErrorBlock.tsx
@@ -4,9 +4,13 @@ import styles from './ErrorBlock.module.sass'
 const ErrorBlock = ({ data }: any) => {
     return (
         <div className={styles.error}>
-            <Icon name='error'/>
-            <br></br>
-            {data.status}
+            <Icon name='error' color={'red'} size={40} />
+            <p className={styles.errorTitle}>Error</p>
+            <p className={styles.errorText}>Error status is {data.status}
+                <br />
+                Message: {data.data.message}
+            </p>
+            <p></p>
         </div>
     )
 }
