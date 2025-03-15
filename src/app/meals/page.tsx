ac487d9fe09d2e061fb608f8acf6763910373c7a
airat-vagapov <airlake@yandex.ru>
2025-03-15 11:48:56 +0300

    suspense wrapper added

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -8,6 +8,7 @@ import Container from '@/components/Container/Container'
 import MealList from '@/components/MealList/MealList'
 import PageTitle from '@/ui/PageTitle/PageTitle'
 import Content from '@/ui/Content/Content'
+import { Suspense } from "react";
 
 const MealsPage = () => {
     const searchParams = useSearchParams()
@@ -16,10 +17,12 @@ const MealsPage = () => {
     return (
         <Provider store={store}>
             <Container>
-                <Content>
-                    <PageTitle title="Meals" desc={'Search result by "' + searchQuery + '"'} />
-                    <MealList searchParams={searchParams} />
-                </Content>
+                <Suspense fallback={<div>Loading</div>}>
+                    <Content>
+                        <PageTitle title="Meals" desc={'Search result by "' + searchQuery + '"'} />
+                        <MealList searchParams={searchParams} />
+                    </Content>
+                </Suspense>
             </Container>
         </Provider>
     )
